853a9eb3f74da83c36e35283087ebf2d1d247e92
sthapa6 <sthapa6@ncsu.edu>
2026-02-14 20:51:23 -0500

    Removed example code

--- background.js
+++ background.js
@@ -5,27 +5,3 @@ chrome.runtime.onInstalled.addListener(() => {
       contexts: ["link"]
     });
   });
-
-chrome.contextMenus.onClicked.addListener((info) => {
-    if (info.menuItemId === "checkLink") {
-        analyzeUrl(info.linkUrl);
-    }
-});
-
-async function analyzeUrl(url) {
-    console.log("Checking:", url);
-    try {
-        const response = await fetch('http://localhost:8000/analyze', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ url: url })
-        });
-        const data = await response.json();
-        
-        if (data.risk_level === 'high') {
-            alert('WARNING: ${data.reason}');
-        }
-    } catch (error) {
-        console.error("Server not running yet!", error);
-    }
-}

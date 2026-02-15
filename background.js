cada1165d31904b6e6802d72ee29914a1d2a5861
sthapa6 <sthapa6@ncsu.edu>
2026-02-14 19:23:50 -0500

    changed background.js to only implement right click analyzer now

--- background.js
+++ background.js
@@ -1,10 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: "OFF",
-    });
-  });
-
-  chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "checkLink",
       title: "Check Link",
@@ -12,11 +6,26 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info) => {
     if (info.menuItemId === "checkLink") {
-      console.log("Checking this URL:", info.linkUrl);
-      alert("Checking link: " + info.linkUrl);
+        analyzeUrl(info.linkUrl);
     }
-  });
+});
 
-
+async function analyzeUrl(url) {
+    console.log("Checking:", url);
+    try {
+        const response = await fetch('http://localhost:8000/analyze', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ url: url })
+        });
+        const data = await response.json();
+        
+        if (data.risk_level === 'high') {
+            alert('WARNING: ${data.reason}');
+        }
+    } catch (error) {
+        console.error("Server not running yet!", error);
+    }
+}
